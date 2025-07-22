2691a69011cd9882b2c0d6a34a2df221f6638b74
Srinidhi Chitti <146361035+Srinidhi-Chitti@users.noreply.github.com>
2025-07-22 22:54:49 +0530

    Add files via upload

---
+++ DAY1/numbermirror.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // Include the standard input-output library
+
+int main() {        // main function starts
+    int num;        // declare an integer variable
+
+    scanf("%d", &num);   // read the input from user
+    printf("%d\n", num); // print the number back
+
+    return 0;       // signal that the program ended successfully
+}
